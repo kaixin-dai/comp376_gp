010cdf23b264b1827a70c2ed68bb973d6076d71e
zhulaodi <joydai19981214@gmail.com>
2020-11-01 15:11:01 -0500

    essence complete

---
+++ Assets/Scripts/Essence.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Essence : MonoBehaviour
+{
+    // Start is called before the first frame update
+    
+    public int mAmount = 0;
+
+    [SerializeField]
+    GameObject mPlayer;
+
+
+    public void Interact()
+    {
+        mPlayer.GetComponent<PlayerResources>().AddEssences(mAmount);
+        Destroy(this.gameObject);
+    }
+
+}
