c959ed43f96b6cac18ca533c669e6365f1fa4e27
zhulaodi <joydai19981214@gmail.com>
2020-11-20 21:13:23 -0500

    implement day/night

---
+++ Assets/Scripts/Environments/Game.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Game : MonoBehaviour
+{
+    // Start is called before the first frame update
+    float time = 0.0f;
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        time = time + Time.deltaTime;
+        if(time > 10){
+            GetComponent<Sun>().StartDay();
+            time = 0;
+            return;
+        }
+    }
+}
---
+++ Assets/Scripts/Environments/Sun.cs
@@ -0,0 +1,87 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sun : MonoBehaviour
+{
+
+    [SerializeField]
+    float nMinsPerLevel;
+    float nTotalSeconds;
+
+    float nStart;
+    float nEnd;
+    float nYOffset;
+    float nCurrentX;
+    float nTimeCounter;
+    bool day;
+    [SerializeField]
+    Light light;
+
+    float nOriginalIntensity;
+
+
+    Vector3 nNightRotation;
+    [SerializeField]
+    float nNightIntensity;
+
+    void Start()
+    {   
+        nOriginalIntensity = light.intensity;
+        nTimeCounter = 0.0f;
+        nStart = 25.0f;
+        nEnd = 155.0f;
+        nYOffset = -30f; 
+        nTotalSeconds = nMinsPerLevel * 60.0f;
+        nNightRotation = new Vector3(90.0f,-30.0f,0.0f);
+        nCurrentX = nStart;
+        day = true;
+    }
+
+    void Update()
+    {
+        if(day)
+        {
+
+            float intropolation = (nEnd - nStart)/nTotalSeconds * Time.deltaTime;
+            nCurrentX = nCurrentX + intropolation;  
+
+            if(nTimeCounter >= nTotalSeconds)
+            {
+                day = false;
+                nTimeCounter = 0.0f;
+                nCurrentX = nStart;
+                light.intensity = nNightIntensity;
+                transform.rotation = Quaternion.Euler(nNightRotation);
+            }
+            else
+            {
+                
+                var currentRotation = new Vector3(nCurrentX, nYOffset, 0.0f);
+                transform.rotation = Quaternion.Euler(currentRotation);
+                nTimeCounter = nTimeCounter + Time.deltaTime;
+            }
+          
+        }
+
+        else
+        {
+            StartDay(); //delete this light to enable  night mode
+        }
+
+
+
+
+
+
+    }
+
+
+    public void StartDay()
+    {
+        light.intensity = nOriginalIntensity;
+        day = true;
+    }
+
+
+}
