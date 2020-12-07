22c638491262c349bd5509bbd837f0913a39b551
tuesd-y <crefollet1@gmail.com>
2020-12-07 03:32:38 -0500

    Tier and cost appear above turret UI when specific turret is selected

--- Assets/Scripts/Turret/BuildManager.cs
+++ Assets/Scripts/Turret/BuildManager.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.EventSystems;
+using TMPro;
 
 public class BuildManager : MonoBehaviour
 {
@@ -21,6 +22,16 @@ public class BuildManager : MonoBehaviour
     public GameObject laserToggle;
     public GameObject shockToggle;
 
+    private GameObject gunnerTier;
+    private GameObject rocketTier;
+    private GameObject laserTier;
+    private GameObject shockTier;
+
+    private GameObject gunnerCost;
+    private GameObject rocketCost;
+    private GameObject laserCost;
+    private GameObject shockCost;
+
     private int turretTier;
 
     Camera mainCam;
@@ -37,13 +48,36 @@ public class BuildManager : MonoBehaviour
     private void Start()
     {
 
-/*    gunnerToggle = GameObject.Find("GunnerTurret");
-    rocketToggle = GameObject.Find("RocketTurret");
-    laserToggle = GameObject.Find("LaserTurret");
-    shockToggle = GameObject.Find("ShockTurret");*/
+        /*    gunnerToggle = GameObject.Find("GunnerTurret");
+            rocketToggle = GameObject.Find("RocketTurret");
+            laserToggle = GameObject.Find("LaserTurret");
+            shockToggle = GameObject.Find("ShockTurret");*/
+
+        // Hide all turret tier and price labels
+        gunnerTier = GameObject.Find("GunnerTurretLevel");
+        laserTier = GameObject.Find("LaserTurretLevel");
+        rocketTier = GameObject.Find("RocketTurretLevel");
+        shockTier = GameObject.Find("ShockTurretLevel");
+
+        gunnerCost = GameObject.Find("GunnerTurretCost");
+        laserCost = GameObject.Find("LaserTurretCost");
+        rocketCost = GameObject.Find("RocketTurretCost");
+        shockCost = GameObject.Find("ShockTurretCost");
+
+        gunnerTier.SetActive(false);
+        laserTier.SetActive(false);
+        rocketTier.SetActive(false);
+        shockTier.SetActive(false);
+
+        gunnerCost.SetActive(false);
+        laserCost.SetActive(false);
+        rocketCost.SetActive(false);
+        shockCost.SetActive(false);
 
 
-    mainCam=Camera.main;
+
+
+        mainCam =Camera.main;
         ray = mainCam.ScreenPointToRay(Input.mousePosition);
 
         turretSelected = TURRETS.None;
@@ -148,6 +182,8 @@ public class BuildManager : MonoBehaviour
         }
 
         money = PlayerResources.GetEssence();
+
+        // By default pressing the 1234 keys will get you the tier 1 turrets
         if (Input.GetKeyDown(KeyCode.Alpha1))
         {
             selectGunner(1);
@@ -170,31 +206,68 @@ public class BuildManager : MonoBehaviour
         if (isOn)
         {
             if (turretTier == 1)
+            {
                 selectedTD = gunner1TurretData;
+                gunnerTier.SetActive(true);
+                gunnerTier.GetComponent<TextMeshProUGUI>().text = "1";
+                gunnerCost.SetActive(true);
+                gunnerCost.GetComponent<TextMeshProUGUI>().text = "£" + gunner1TurretData.cost.ToString();
+            }
             if (turretTier == 2)
+            {
                 selectedTD = gunner2TurretData;
+                gunnerTier.SetActive(true);
+                gunnerTier.GetComponent<TextMeshProUGUI>().text = "2";
+                gunnerCost.SetActive(true);
+                gunnerCost.GetComponent<TextMeshProUGUI>().text = "£" + gunner2TurretData.cost.ToString();
+            }
             if (turretTier == 3)
+            {
                 selectedTD = gunner3TurretData;
+                gunnerTier.SetActive(true);
+                gunnerTier.GetComponent<TextMeshProUGUI>().text = "3";
+                gunnerCost.SetActive(true);
+                gunnerCost.GetComponent<TextMeshProUGUI>().text = "£" + gunner3TurretData.cost.ToString();
+            }
         }
         else
         {
             selectedTD = null;
+            gunnerTier.SetActive(false);
+            gunnerCost.SetActive(false);
         }
     }
     public void onRocketTurretSelected(bool isOn)
     {
         if (isOn)
         {
-            if (turretTier == 1)
+            if (turretTier == 1) { 
                 selectedTD = rocket1TurretData;
-            if (turretTier == 2)
+                rocketTier.SetActive(true);
+                rocketTier.GetComponent<TextMeshProUGUI>().text = "1";
+                rocketCost.SetActive(true);
+                rocketCost.GetComponent<TextMeshProUGUI>().text = "£" + rocket1TurretData.cost.ToString();
+            }
+            if (turretTier == 2) { 
+                selectedTD = rocket2TurretData;
+                rocketTier.SetActive(true);
+                rocketTier.GetComponent<TextMeshProUGUI>().text = "2";
+                rocketCost.SetActive(true);
+                rocketCost.GetComponent<TextMeshProUGUI>().text = "£" + rocket2TurretData.cost.ToString();
+            }
+            if (turretTier == 3) { 
                 selectedTD = rocket3TurretData;
-            if (turretTier == 3)
-                selectedTD = rocket3TurretData;
+                rocketTier.SetActive(true);
+                rocketTier.GetComponent<TextMeshProUGUI>().text = "3";
+                rocketCost.SetActive(true);
+                rocketCost.GetComponent<TextMeshProUGUI>().text = "£" + rocket3TurretData.cost.ToString();
+            }
         }
         else
         {
             selectedTD = null;
+            rocketTier.SetActive(false);
+            rocketCost.SetActive(false);
         }
 
     }
@@ -203,15 +276,33 @@ public class BuildManager : MonoBehaviour
         if (isOn)
         {
             if (turretTier == 1)
+            {
                 selectedTD = laser1TurretData;
-            if (turretTier == 2)
+                laserTier.SetActive(true);
+                laserTier.GetComponent<TextMeshProUGUI>().text = "1";
+                laserCost.SetActive(true);
+                laserCost.GetComponent<TextMeshProUGUI>().text = "£" + laser1TurretData.cost.ToString();
+            }
+            if (turretTier == 2) { 
                 selectedTD = laser2TurretData;
-            if (turretTier == 3)
+                laserTier.SetActive(true);
+                laserTier.GetComponent<TextMeshProUGUI>().text = "2";
+                laserCost.SetActive(true);
+                laserCost.GetComponent<TextMeshProUGUI>().text = "£" + laser2TurretData.cost.ToString();
+            }
+            if (turretTier == 3) { 
                 selectedTD = laser3TurretData;
+                laserTier.SetActive(true);
+                laserTier.GetComponent<TextMeshProUGUI>().text = "3";
+                laserCost.SetActive(true);
+                laserCost.GetComponent<TextMeshProUGUI>().text = "£" + laser3TurretData.cost.ToString();
+            }
         }
         else
         {
             selectedTD = null;
+            laserTier.SetActive(false);
+            laserCost.SetActive(false);
         }
     }
     public void onShockTurretSelected(bool isOn)
@@ -219,15 +310,33 @@ public class BuildManager : MonoBehaviour
         if (isOn)
         {
             if (turretTier == 1)
+            {
                 selectedTD = shock1turretData;
-            if (turretTier == 2)
+                shockTier.SetActive(true);
+                shockTier.GetComponent<TextMeshProUGUI>().text = "1";
+                shockCost.SetActive(true);
+                shockCost.GetComponent<TextMeshProUGUI>().text = "£" + shock1turretData.cost.ToString();
+            }
+            if (turretTier == 2) { 
                 selectedTD = shock2turretData;
-            if (turretTier == 3)
+                shockTier.SetActive(true);
+                shockTier.GetComponent<TextMeshProUGUI>().text = "2";
+                shockCost.SetActive(true);
+                shockCost.GetComponent<TextMeshProUGUI>().text = "£" + shock2turretData.cost.ToString();
+            }
+            if (turretTier == 3) { 
                 selectedTD = shock3turretData;
+                shockTier.SetActive(true);
+                shockTier.GetComponent<TextMeshProUGUI>().text = "3";
+                shockCost.SetActive(true);
+                shockCost.GetComponent<TextMeshProUGUI>().text = "£" + shock3turretData.cost.ToString();
+            }
         }
         else
         {
             selectedTD = null;
+            shockTier.SetActive(false);
+            shockCost.SetActive(false);
         }
     }
     public void BuildTurret(GameObject turretPrefab)
@@ -296,8 +405,8 @@ public class BuildManager : MonoBehaviour
         else
         {
             rocketToggle.GetComponent<Toggle>().isOn = true;
-            /*                turretSelected = TURRETS.Rocket;
-                            laserToggle.SetActive(false);
+            turretSelected = TURRETS.Rocket;
+                           /* laserToggle.SetActive(false);
                             gunnerToggle.SetActive(false);
                             rocketToggle.SetActive(true);
                             shockToggle.SetActive(false);*/
